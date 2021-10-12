2d8cc75d8746006dc248f703f58cbb1a1aebdcb3
HN410 <HN410>
2021-10-12 21:58:24 +0900

    add BaseNumber

---
+++ src/main/java/com/utils/BaseNumber.java
@@ -0,0 +1,13 @@
+package com.utils;
+
+public enum BaseNumber {
+    HEX(16), 
+    DEC(10), 
+    OCT(8), 
+    BIN(2);
+    
+    private final int value;
+    private BaseNumber(final int value){
+        this.value = value;
+    }
+}
