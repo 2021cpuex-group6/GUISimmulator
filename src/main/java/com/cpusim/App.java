cd7142bb5bf17f747d502dcaf5938a2074ce42be
HN410 <HN410>
2021-11-06 16:58:43 +0900

    add CheckConstant

--- src/main/java/com/cpusim/App.java
+++ src/main/java/com/cpusim/App.java
@@ -10,10 +10,12 @@ import javax.swing.JTextField;
 
 import com.MainWindow.MainWindow;
 import com.MainWindow.PropertiesClass;
+import com.utils.ConstantsClass;
 public class App 
 {
     public static void main( String[] args )
     {
+        ConstantsClass.checkConstants();
         Properties properties = PropertiesClass.getProperties();
         MainWindow main = new MainWindow(properties);
         main.setVisible(true);
--- src/main/java/com/utils/ConstantsClass.java
+++ src/main/java/com/utils/ConstantsClass.java
@@ -11,7 +11,19 @@ public class ConstantsClass {
     public final static int REGISTER_KINDS = 4; // レジスタの種別
     public final static String INTEGER_REGISTER_PREFIX = "x";
     public final static String FLOAT_REGISTER_PREFIX = "f";
-    public final static int MEMORY_BYTE_N = 0x1000; //2^12バイト
-    public final static int MEMORY_SHOW_LINE_N = 0x100; // 2^8 * 8バイト表示 
+    public final static int MEMORY_BYTE_N = 0x10000; //2^16バイト
+    public final static int MEMORY_SHOW_LINE_N = 0x200; // 2^9 * 8バイト表示 
     public final static int MEMORY_COLUMN_N = 8;
+
+    private final static String INVALID_CONSTANT_ERROR  = "定数が制約を満たしていません";
+
+    public  static void checkConstants(){
+        // 定数が制約を満たしているか確認
+        boolean res = false;
+        res  = res || ((MEMORY_SHOW_LINE_N  * MEMORY_COLUMN_N) == 0x1000);
+        res = res || ((MEMORY_BYTE_N % (MEMORY_SHOW_LINE_N * MEMORY_COLUMN_N)) == 0);
+        if(res){
+            ErrorChecker.errorCheck(INVALID_CONSTANT_ERROR);
+        }
+    }
 }
